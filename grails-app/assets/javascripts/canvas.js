5b5c77f1c7f3dd5dc79946aaf983369a4d9e801a
Jordon Mears <jordoncm@gmail.com>
2014-12-20 15:56:57 -0500

    Removed dead code.

--- grails-app/assets/javascripts/canvas.js
+++ grails-app/assets/javascripts/canvas.js
@@ -8,19 +8,4 @@ $(function() {
   (new ocm.views.Map()).render();
   new ocm.Router();
   Backbone.history.start();
-
-  /*
-  var socket = new SockJS(ocm.URL_BASE + '/stomp');
-  var client = Stomp.over(socket);
-
-  client.connect({}, function() {
-    client.subscribe('/topic/hello', function(message) {
-      console.log(JSON.parse(message.body));
-    });
-  });
-  var go = function() {
-    client.send('/topic/hello', {}, JSON.stringify({foo: 'bar'}));
-  };
-  setTimeout(go, 2000);
-  */
 });
