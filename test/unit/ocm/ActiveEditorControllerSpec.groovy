117342a5d0e9cb27d0a7b1bbf033f5000841ecb8
Jordon Mears <jordoncm@gmail.com>
2014-12-23 16:43:50 -0500

    Starting to add tests.

--- test/unit/ocm/ActiveEditorControllerSpec.groovy
+++ test/unit/ocm/ActiveEditorControllerSpec.groovy
@@ -4,17 +4,79 @@ import grails.test.mixin.TestFor
 import spock.lang.Specification
 
 /**
- * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for
+ * usage instructions.
  */
 @TestFor(ActiveEditorController)
+@Mock([ActiveEditor, Map])
 class ActiveEditorControllerSpec extends Specification {
+  Map map
 
-    def setup() {
-    }
+  def setup() {
+    map = new Map(name: 'Test Map')
+    map.save()
+  }
 
-    def cleanup() {
-    }
+  def cleanup() {
+    map.delete()
+  }
 
-    void "test something"() {
-    }
+  void 'Ensure register creates an editor and ties it to the map.'() {
+    when:
+      def result = controller.register(map.id)
+    then:
+      result['id'] != null
+      result['map'] == map.id
+  }
+
+  void 'Ensure multiple editors are not given the same id.'() {
+    when:
+      def firstResult = controller.register(map.id)
+      def secondResult = controller.register(map.id)
+    then:
+      firstResult['id'] != secondResult['id']
+  }
+
+  void 'Ensure multiple calls to register on the same map increments the editor count.'() {
+    when:
+      def firstResult = controller.register(map.id)
+      def secondResult = controller.register(map.id)
+    then:
+      firstResult['editors'] == 1
+      secondResult['editors'] == 2
+  }
+
+  void 'Ensure calls to register for different maps does not increment the wrong editor count.'() {
+    when:
+      def secondMap = new Map(name: 'Second Map')
+      secondMap.save()
+
+      def firstResult = controller.register(map.id)
+      def secondResult = controller.register(secondMap.id)
+    then:
+      firstResult['map'] != secondResult['map']
+      firstResult['editors'] == 1
+      secondResult['editors'] == 1
+
+      secondMap.delete()
+  }
+
+  void 'Ensure register updates last check in.'() {
+    when:
+      def registerResult = controller.register(map.id)
+    then:
+      def editor = ActiveEditor.get(registerResult['id'])
+      editor.lastCheckIn > 0
+  }
+
+  void 'Ensure heartbeat updates last check in.'() {
+    when:
+      def registerResult = controller.register(map.id)
+      def editor = ActiveEditor.get(registerResult['id'])
+      def intialCheckIn = editor.lastCheckIn
+      // controller.heartbeat(['id': editor.id, 'mapId': map.id])
+    then:
+      editor.lastCheckIn > 0
+
+  }
 }
--- test/unit/ocm/RectangleSpec.groovy
+++ test/unit/ocm/RectangleSpec.groovy
@@ -6,8 +6,8 @@ import spock.lang.Specification
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
-@TestFor(View)
-class ViewSpec extends Specification {
+@TestFor(Rectangle)
+class RectangleSpec extends Specification {
 
     def setup() {
     }
